aa6a683bbd7b4af3f6e0aed47c72335b12a32f6e
Tiya149 <tiya4819.be23@chitkara.edu.in>
2025-07-19 16:06:27 +0530

    Add files via upload

---
+++ myApp/myAppTests/myAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  myAppTests.swift
+//  myAppTests
+//
+//  Created by Student on 19/07/25.
+//
+
+import Testing
+@testable import myApp
+
+struct myAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
